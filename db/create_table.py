d7ce74aa6031bd8b83d18595825b2c34f3da33fb
jacksyen <hyqiu.syen@gmail.com>
2014-12-09 17:50:18 +0800

    优化静态方法

--- db/create_table.py
+++ db/create_table.py
@@ -15,18 +15,21 @@ class Create:
     '''
     获取全局配置表sql
     '''
-    def gk7_douban_global(self):
+    @staticmethod
+    def gk7_douban_global():
         return 'CREATE TABLE IF NOT EXISTS %s(smtp text, smtp_port text, email_user text, email_pwd text, email_encode text, addtime datetime, updatetime datetime)' %(Global.GLOBAL_DB_TBL_GLOBAL_NAME)
 
     '''
     获取等待发送邮件表sql
     email_send_status: 邮件发送状态[WAIT:等待发送, COMPLETE:发送完成, ERROR:发送异常]
     '''
-    def gk7_douban_wait_emails(self):
+    @staticmethod
+    def gk7_douban_wait_emails():
         return 'CREATE TABLE IF NOT EXISTS %s(email_to_user text, email_attach_file text, email_title text, email_auth text, email_send_status text, addtime datetime, updatetime datetime' %(Global.GLOBAL_DB_TBL_WAIT_EMAILS_NAME)
 
     '''
     获取所有书籍表sql
     '''
-    def gk7_douban_books(self):
+    @staticmethod
+    def gk7_douban_books():
         return 'CREATE TABLE IF NOT EXISTS %s(book_number text, book_title text, book_subtitle text, book_author text, book_file_path text, addtime datetime, updatetime datetime)' %(Global.GLOBAL_DB_TBL_BOOK_NAME)
