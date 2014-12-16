edf26ee1635aa710967219aeb658201a867010fc
jacksyen <hyqiu.syen@gmail.com>
2014-12-16 17:37:44 +0800

    添加切面日志信息

--- helper/aop.py
+++ helper/aop.py
@@ -8,11 +8,39 @@ author by jacksyen[hyqiu.syen@gmail.com]
 import time
 from log import logger
 
+'''
+打印调用函数信息，包括参数，运行时间
+'''
 def exec_time(func):
     def wrapper(*args, **args2):
         time.clock()
         logger.info(u"调用函数:{%s.%s}，入参：%s" %(func.__module__, func.__name__, str(args)))
         back = func(*args, **args2)
         logger.info("调用函数:{%s.%s}，出参：%s，耗时：%.3fs" %(func.__module__, func.__name__, str(back), time.clock()))
+        return back
+    return wrapper
+
+'''
+打印调用函数输入信息，包括调用信息
+输出信息，包括参数，运行时间
+'''
+def exec_out_time(func):
+    def wrapper(*args, **args2):
+        time.clock()
+        logger.info(u"调用函数:{%s.%s}" %(func.__module__, func.__name__))
+        back = func(*args, **args2)
+        logger.info("调用函数:{%s.%s}，出参：%s，耗时：%.3fs" %(func.__module__, func.__name__, str(back), time.clock()))
+        return back
+    return wrapper
+
+'''
+打印调用函数耗时信息
+'''
+def exec_time_consum(func):
+    def wrapper(*args, **args2):
+        time.clock()
+        logger.info(u"调用函数:{%s.%s}" %(func.__module__, func.__name__))
+        back = func(*args, **args2)
+        logger.info("调用函数:{%s.%s}，耗时：%.3fs" %(func.__module__, func.__name__, time.clock()))
         return back
     return wrapper
