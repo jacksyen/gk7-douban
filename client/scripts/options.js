80f6ac0bc485be677004133950b81380d3c06104
jacksyen <hyqiu.syen@gmail.com>
2014-12-16 23:03:11 +0800

    update client script format

--- client/scripts/options.js
+++ client/scripts/options.js
@@ -1,18 +1,18 @@
 $(document).ready(function (){
-	var toEmail = localStorage.TO_MAIL;
-	if (toEmail){
-		var temp = toEmail.split('@');
-		if(temp.length > 1){
-			$('#userEmail').val(temp[0]);
-			$('#emailDomain').val(temp[1]);
-		}
+    var toEmail = localStorage.TO_MAIL;
+    if (toEmail){
+	var temp = toEmail.split('@');
+	if(temp.length > 1){
+	    $('#userEmail').val(temp[0]);
+	    $('#emailDomain').val(temp[1]);
 	}
-	
-	$('.button').click(function (){
-		localStorage.TO_MAIL = $('#userEmail').val() + '@' + $('#emailDomain').val();
-		$('.msg').html('保存成功');
-		$('.msg').fadeTo(3000, 0.50, function (){
-			$('.msg').html('');
-		});
+    }
+    
+    $('.button').click(function (){
+	localStorage.TO_MAIL = $('#userEmail').val() + '@' + $('#emailDomain').val();
+	$('.msg').html('保存成功');
+	$('.msg').fadeTo(3000, 0.50, function (){
+	    $('.msg').html('');
 	});
+    });
 });
