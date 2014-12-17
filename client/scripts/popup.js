deea83a616d341c780d6f1b88eb83197cddc35d5
jacksyen <hyqiu.syen@gmail.com>
2014-12-17 17:42:34 +0800

    优化前端等待

--- client/scripts/popup.js
+++ client/scripts/popup.js
@@ -1,3 +1,11 @@
+paceOptions = {
+    ajax: false, // disabled
+    document: false, // disabled
+    eventLag: false, // disabled
+    elements: {
+        selectors: ['.gk7-douban-send-end']
+    }
+};
 document.addEventListener('DOMContentLoaded', function () {
     
     chrome.extension.sendMessage('id',{'sa':'12312'}, function (response){
