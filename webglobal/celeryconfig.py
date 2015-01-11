5c050f1d924ef2ad783ed09ebad595dc969f0c8c
jacksyen <hyqiu.syen@gmail.com>
2015-01-11 11:47:51 +0800

    update celery

--- webglobal/celeryconfig.py
+++ webglobal/celeryconfig.py
@@ -7,5 +7,27 @@ celery配置
 """
 CELERY_ENABLE_UTC = True
 CELERY_TIMEZONE = 'Europe/London'
+
+#BROKER_URL = 'amqp'
 CELERY_RESULT_BACKEND = 'amqp'
-CELERY_TASK_RESULT_EXPIRES = 18000  # 5 hours.
+
+# 任务结果超时时间(秒)
+CELERY_TASK_RESULT_EXPIRES = 24*60*60  # 24hours.
+
+# email设置
+ADMINS = (
+    ('Jack Syen', 'hyqiu.syen@gmail.com'),
+)
+
+# 发送者邮箱设置
+SERVER_MAIL = '461166828@qq.com'
+EMAIL_HOST = 'smtp.qq.com'
+EMAIL_PORT = 25
+EMAIL_HOST_USER = '461166828'
+EMAIL_HOST_PASSWORD = ''
+
+
+
+
+
+
