c7ca488b0ed41cdd3f587b75b72e5781e5d1d029
jacksyen <hyqiu.syen@gmail.com>
2015-02-01 21:19:08 +0800

    update client push url

--- client/scripts/background.js
+++ client/scripts/background.js
@@ -55,8 +55,8 @@ function send(request, callback){
 	'ebookId': request.ebookId
     };
     $.ajax({
-	//url: 'http://gk7.pw:8000/send',
-        url: 'http://localhost:8000/send',
+	url: 'http://gk7.pw:8000/send',
+        //url: 'http://localhost:8000/send',
         //url: 'http://192.168.3.167:8000/send',
 	data: articleData,
 	dataType: 'json',
