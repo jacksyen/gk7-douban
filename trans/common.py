62795d6a81544917ae8697bce271ac94c7b8be40
jacksyen <hyqiu.syen@gmail.com>
2015-08-29 15:23:23 +0800

    fixed bug

--- trans/common.py
+++ trans/common.py
@@ -7,7 +7,7 @@ import webglobal.globals as gk7
 class Common():
 
     @staticmethod
-    def send_mail(self, send_mail_type, email_id, attach_file, to_email, book_title, book_author):
+    def send_mail(send_mail_type, email_id, attach_file, to_email, book_title, book_author):
         if send_mail_type == 'gmail':
             import urllib, urllib2
             data = {
@@ -20,14 +20,14 @@ class Common():
             result = urllib2.urlopen(gk7.API_URL.get('mail'), urllib.urlencode(data)).read()
             if not result:
                 logger.error(u'发送邮件结果为空')
-                return false
+                return False
             json_result = json.loads(result)
             if str(json_result.get('status')) != gk7.API_STATUS.get('success'):
                 logger.error(u'发送邮件失败,API接口返回：%s' % str(result))
-                return false
-            return true
+                return False
+            return True
         # 其他类型
         # 发送邮件并修改待发送邮件状态
         from helper.tasks import MailTask
         MailTask.send.delay(email_id, attach_file, to_email, book_title, book_author)
-        return true
+        return True
