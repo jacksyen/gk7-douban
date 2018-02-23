646b2c411936117f6718e8846b615f1bc3068bfb
jacksyen <hyqiu.syen@gmail.com>
2018-02-23 11:33:41 +0800

    添加转换参数

--- helper/proc.py
+++ helper/proc.py
@@ -8,7 +8,7 @@ author by jacksyen[hyqiu.syen@gmail.com]
 import os
 
 from log import logger
-from subprocess import call
+import subprocess
 import helper.aop as aop
 import webglobal.globals as gk7
 
@@ -36,6 +36,7 @@ class proc_helper:
         out_file_path = '%s/%s.%s' %(out_file_dir, file_name[0: file_name.rfind('.')], gk7.OUT_FILE_FORMAT)
         ## 
         ## 说明：
+        #'/opt/app/gk7-douban/data/39709928/26064/1519349229.html', u'/data/gk7-douban/mobi/39709928/26064', u'\u987d\u77f3', u'/opt/app/gk7-douban/data/cover/39709928.jpg')
         ## 调用系统命令：ebook-convert input_file out_file --authors <author> --cover <img> --language zh --chapter-mark "none" --page-breaks-before '//*[@class="pagebreak"]'
         ## --cover 书籍封面
         ## --chapter-mark 设置标注章节的模式，none：不会在章节前插入控制
@@ -56,12 +57,20 @@ class proc_helper:
         params.append('UTF-8')
         params.append('--chapter-mark')
         params.append('none')
-        params.append('--page-breaks-before')
-        params.append('\'//*[@class="%s"]\'' %gk7.BOOK_PAGE_SPLIT)
+        params.append('--mobi-toc-at-start')
+        params.append('--pretty-print')
+        #params.append('--page-breaks-before')
+        #params.append('"//*[@class="%s"]"' %gk7.BOOK_PAGE_SPLIT)
 
-        
         ## 调用
-        call(params)
+        p = subprocess.Popen(params, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        ret = p.wait()
+        logger.info('ret:%s' %ret)
+        logger.info(p.stdout.read())
+        logger.info(p.stderr.read())
+        #retcode = call(params)
+        #logger.info('retcode:%s' %retcode)
+
         #call(['ebook-convert', input_file_path, out_file_path, '--authors', author, '--cover', cover, '--chapter-mark', 'none', '--page-breaks-before', '//*[@class="%s"]' %gk7.BOOK_PAGE_SPLIT])
         ## 转换成功
         return out_file_path
