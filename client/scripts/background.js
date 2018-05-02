5bbc427221cdb772fdf0c7a1323c471ca3bec074
jacksyen <hyqiu.syen@gmail.com>
2018-05-02 11:58:48 +0800

    修改服务url

--- client/scripts/background.js
+++ client/scripts/background.js
@@ -50,9 +50,9 @@ function set_icon(tab_id, icon) {
 **/
 function send(request, callback){
     $.ajax({
-      //url: 'http://gk7.pw:8000/send',
+      url: 'http://gk7.pw:8000/send',
 	//url: 'http://112.124.38.237:8000/send',
-      url: 'http://localhost:8080/send',
+      //url: 'http://localhost:8080/send',
         //url: 'http://192.168.1.108:8000/send',
 	data: {
           'tmplId': request.tmplId,
