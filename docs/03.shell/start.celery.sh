62df3e0edde01a99c95c5d7aabee3811e0500565
jacksyen <hyqiu.syen@gmail.com>
2019-11-18 22:53:08 +0800

    shell bug fixed

--- docs/03.shell/start.celery.sh
+++ docs/03.shell/start.celery.sh
@@ -4,5 +4,5 @@ ps -ef|grep celery |grep -v grep |grep -v sh| awk '{print $2}'| xargs kill -9
 
 unset PYTHONPATH
 cd ../../gk7/
-
+mkdir logs
 ../env/bin/celery -A task.tasks worker -l info -D -f logs/gk7-douban.log --pidfile=logs/celery.pid
