043af75be6e9df0dca6c487d67d0bdc9dc15b602
jacksyen <hyqiu.syen@gmail.com>
2019-11-19 17:44:49 +0800

    适配python3

--- docs/03.shell/start.web.sh
+++ docs/03.shell/start.web.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 cd ../../
+source env/bin/activate
 uwsgi --socket 127.0.0.1:9031 --wsgi-file gk7/index.py --master --enable-threads --processes 2 --daemonize gk7/logs/gk7-douban.log --pidfile gk7/logs/uwsgi.pid
